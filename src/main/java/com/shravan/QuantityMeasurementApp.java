9f22a82677f52a90b653dc1c601a227a3b2b50ac
3-shravan <shantanuyadav379@gmail.com>
2026-02-19 12:39:48 +0530

    [Sravan Kumar] Add. Quantity Measurement App: uc 2 Inch measurement equality

--- src/main/java/com/shravan/QuantityMeasurementApp.java
+++ src/main/java/com/shravan/QuantityMeasurementApp.java
@@ -2,10 +2,41 @@ package com.shravan;
 
 public class QuantityMeasurementApp {
 
-  public boolean areEqual(double firstValueInFeet, double secondValueInFeet) {
+  public boolean areFeetEqual(double firstValueInFeet, double secondValueInFeet) {
     Feet firstFeet = new Feet(firstValueInFeet);
     Feet secondFeet = new Feet(secondValueInFeet);
     return firstFeet.equals(secondFeet);
+  }
+
+  public boolean areInchesEqual(double firstValueInInches, double secondValueInInches) {
+    Inches firstInches = new Inches(firstValueInInches);
+    Inches secondInches = new Inches(secondValueInInches);
+    return firstInches.equals(secondInches);
+  }
+
+  public static void demonstrateFeetEquality() {
+    QuantityMeasurementApp app = new QuantityMeasurementApp();
+    double firstFeet = 1.0;
+    double secondFeet = 1.0;
+
+    boolean result = app.areFeetEqual(firstFeet, secondFeet);
+    System.out.println("Input: " + firstFeet + " ft and " + secondFeet + " ft");
+    System.out.println("Output: Equal (" + result + ")");
+  }
+
+  public static void demonstrateInchesEquality() {
+    QuantityMeasurementApp app = new QuantityMeasurementApp();
+    double firstInches = 1.0;
+    double secondInches = 1.0;
+
+    boolean result = app.areInchesEqual(firstInches, secondInches);
+    System.out.println("Input: " + firstInches + " inch and " + secondInches + " inch");
+    System.out.println("Output: Equal (" + result + ")");
+  }
+
+  public static void main(String[] args) {
+    demonstrateFeetEquality();
+    demonstrateInchesEquality();
   }
 
   public static final class Feet {
@@ -30,6 +61,32 @@ public class QuantityMeasurementApp {
       }
 
       Feet other = (Feet) obj;
+      return Double.compare(this.value, other.value) == 0;
+    }
+  }
+
+  public static final class Inches {
+    private final double value;
+
+    public Inches(double value) {
+      this.value = value;
+    }
+
+    public double getValue() {
+      return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (this == obj) {
+        return true;
+      }
+
+      if (obj == null || getClass() != obj.getClass()) {
+        return false;
+      }
+
+      Inches other = (Inches) obj;
       return Double.compare(this.value, other.value) == 0;
     }
   }
--- src/test/java/com/shravan/QuantityMeasurementAppTest.java
+++ src/test/java/com/shravan/QuantityMeasurementAppTest.java
@@ -6,6 +6,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import com.shravan.QuantityMeasurementApp.Feet;
+import com.shravan.QuantityMeasurementApp.Inches;
 
 public class QuantityMeasurementAppTest {
 
@@ -42,6 +43,43 @@ public class QuantityMeasurementAppTest {
   @Test
   public void testFeetEquality_SameReference() {
     Feet value = new Feet(1.0);
+
+    assertTrue(value.equals(value));
+  }
+
+  @Test
+  public void testInchesEquality_SameValue() {
+    Inches firstValue = new Inches(1.0);
+    Inches secondValue = new Inches(1.0);
+
+    assertTrue(firstValue.equals(secondValue));
+  }
+
+  @Test
+  public void testInchesEquality_DifferentValue() {
+    Inches firstValue = new Inches(1.0);
+    Inches secondValue = new Inches(2.0);
+
+    assertFalse(firstValue.equals(secondValue));
+  }
+
+  @Test
+  public void testInchesEquality_NullComparison() {
+    Inches value = new Inches(1.0);
+
+    assertFalse(value.equals(null));
+  }
+
+  @Test
+  public void testInchesEquality_DifferentClass() {
+    Inches value = new Inches(1.0);
+
+    assertFalse(value.equals("1.0"));
+  }
+
+  @Test
+  public void testInchesEquality_SameReference() {
+    Inches value = new Inches(1.0);
 
     assertTrue(value.equals(value));
   }
